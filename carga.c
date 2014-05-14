2e31676deb27b65b4241de49a5aded63c031df0b
María Guillermina Véscovo <guilleves@gmail.com>
2014-05-14 14:30:47 -0300

    Create carga.c

---
+++ carga.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+char* carga(int*);
+
+void main(){
+    int n;
+    char *texto;
+    
+    printf("Ingrese el texto\n\n");
+    texto = carga(&n);
+    
+    printf("el texto es %s", texto);
+
+    }
+
+char* carga(int* n){
+    static char texto[1000];
+    int i = 0;
+
+    while ((texto[i] = getchar()) != EOF && i<999);
+        i++;
+        
+    texto[i] = '\0';
+    *n = i;
+
+    return(texto);
+    }
+
+
