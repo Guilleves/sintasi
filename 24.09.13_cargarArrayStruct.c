a79ec40f66e3184ab1a8f0778e0f278b9f770af8
María Guillermina Véscovo <guilleves@gmail.com>
2014-05-17 13:13:04 -0300

    Update 24.09.13_cargarArrayStruct.c

--- 24.09.13_cargarArrayStruct.c
+++ 24.09.13_cargarArrayStruct.c
@@ -6,28 +6,27 @@ struct palcalif *cargaPalabras(int *);
 
 void main(){
     int n;
-    struct palcalif *palabras;
+    struct palcalif *palabrasCargadas;
 
     printf("ingresar palabras\n\n");
-    palabras = cargaPalabras(&n);}
+    palabrasCargadas = cargaPalabras(&n);}
 
 
 
 struct palcalif *cargaPalabras(int *n){
 	static struct palcalif conjuntoPalabras[1000];
-	int i = 0, j = 0; /*i se mueve en los lugares de la struct y j en los de la palabra*/
-	char rta;
+	int i = 0, j = 0; /*i se mueve en los lugares del array de struct y j en los de la palabra*/
+	char rta = 's';
 
 	while (rta != 'n' && i<999){
-		j = 0;
-		while ((conjuntoPalabras[i].pala[j] = getchar()) != EOF && j<20){
+        j = 0;
+		while ((conjuntoPalabras[i].pala[j] = getchar()) != EOF && j<19)
 			j++;
 		conjuntoPalabras[i].pala[j] = '\0';
 		printf("la palabra ingresada fue %s\n\n", conjuntoPalabras[i].pala);
 		i++;
 		printf("Desea continuar ingresando palabras?");
 		scanf("%c", &rta);
-		}
 	}
 	*n = i;
 	return(conjuntoPalabras);
